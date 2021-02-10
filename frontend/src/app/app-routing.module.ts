442faeb899bd89013b122e3bc841c5209cfbeaf6
klylylydeee <klylylydeee@gmail.com>
2021-02-10 20:35:30 +0800

    add components and component routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Created Components
+import { HomeComponent } from './Components/home/home.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { LoginComponent } from './Components/login/login.component';
+import { PostsComponent } from './Components/posts/posts.component';
+
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent},
+  { path: 'Signup', component: SignupComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: 'Posts', component: PostsComponent },
+  { path: '**', redirectTo: 'Home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Created Components
+import { NavigationComponent } from './Components/navigation/navigation.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { PostsComponent } from './Components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
