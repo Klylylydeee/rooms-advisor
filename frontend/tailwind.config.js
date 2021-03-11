e4870e42528cb59ff9e0ac280fdfac54241d710e
klylylydeee <klylylydeee@gmail.com>
2021-03-11 23:22:32 +0800

    add custom font and color to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,9 +5,31 @@ module.exports = {
         './src/**/*.{html,ts}',
       ]
     },
-    darkMode: 'class', // or 'media' or 'class'
+    darkMode: 'class',
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          mango: '#ffc324',
+          mango500: '#F0AE00',
+          aqua: '#2460FF',
+          navy: '#2460ff',
+          orange: '#ff5524'
+        },
+        fontFamily: {
+          montserrat: ['Montserrat'],
+          roboto: ['Roboto'],
+          arvo: ['Arvo'],
+          oswald: ['Oswald'],
+          mulish: ['Mulish'],
+        },
+        screens: {
+          'max-xl': {'max': '1440px'},
+          'max-lg': {'max': '1024px'},
+          'max-md': {'max': '768px'},
+          'max-sm': {'max': '425px'},
+          'max-xs': {'max': '320px'}
+        }
+      }
     },
     variants: {
       extend: {},
